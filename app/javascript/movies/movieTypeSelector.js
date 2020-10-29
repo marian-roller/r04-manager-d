9f49029742e90599d8562077cbccfcfa644555b9
marian-roller <webmaster@rollercoastery.pl>
2020-10-29 20:37:06 +0100

    improvement of dynamic rendering year_end field group in movie form

--- app/javascript/movies/movieTypeSelector.js
+++ app/javascript/movies/movieTypeSelector.js
@@ -1,46 +1,33 @@
 export class MovieTypeSelector {
 
-    constructor() {
+    constructor () {
         this.type = document.getElementById('movie_movie_type');
         this.year_start = document.getElementById('year-start-field-group');
         this.year_end = document.getElementById('year-end-field-group');
         this.movie_type_selector = document.getElementById('movie_movie_type');
-        this.year_end_height = this.year_end.offsetHeight;
         this.renderFieldsUponMovieType();
         this.movie_type_selector.addEventListener('change', (e) => {
             return this.renderFiledsUponMovieTypeChange(e.target.value);
         })
-
     }
 
     // render onchange selector
     renderFiledsUponMovieTypeChange (value) {
-        if ( !this.renderFieldsUponMovieType() ) {
-            // this.year_end.style.height = "0px";
-            this.year_end.classList.remove('d-none');
-            // const id = setInterval(this.frame, 50);
-            // this.year_end.style.background = "yellow";
-            // this.year_end.style.height = this.year_end_height + "px";
-            // console.log(this.year_end.offsetHeight);
+        if (!this.renderFieldsUponMovieType()) {
+            this.year_end.classList.remove('input-wrapper-closed');
+            this.year_end.classList.add('input-wrapper-open');
             this.year_start.childNodes[1].innerText = "Year Start";
         }
     }
 
-    // frame() {
-    //     let h = 0;
-    //     if (h == 61) {
-    //         clearInterval(id);
-    //     } else {
-    //         h++;
-    //         this.year_end.style.height = h + 'px';
-    //     }
-    // }
-
     // render onload form
-    renderFieldsUponMovieType() {
+    renderFieldsUponMovieType () {
+        // if not tvserie
         if (this.type.value != 2) {
-            this.year_end.classList.add('d-none');
+            this.year_end.classList.remove('input-wrapper-open');
+            this.year_end.classList.add('input-wrapper-closed');
             this.year_start.childNodes[1].innerText = "Year";
+            this.year_end.childNodes[3].value = "";
             return true;
         }
     }
