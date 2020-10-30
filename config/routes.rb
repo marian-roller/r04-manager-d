226ed48923712f9f159205bd2e25c52ee2b8ed11
marian-roller <webmaster@rollercoastery.pl>
2020-10-31 00:36:10 +0100

    movie type selector functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :movies
+  get 'onlymovies', to: 'movies#onlyMovies'
+  get 'onlytvseries', to: 'movies#onlyTvseries'
   get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
