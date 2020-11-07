dfef3e77f22300ff5be47ee8818b1da90a9a0df2
marian-roller <webmaster@rollercoastery.pl>
2020-11-07 21:54:00 +0100

    dynamic momovie form fields with js started

--- app/javascript/movies/movieTypeSelector.js
+++ app/javascript/movies/movieTypeSelector.js
@@ -5,6 +5,7 @@ export class MovieTypeSelector {
         this.year_start = document.getElementById('year-start-field-group');
         this.year_end = document.getElementById('year-end-field-group');
         this.movie_type_selector = document.getElementById('movie_movie_type');
+        this.duration = document.getElementById('duration-field-group');
         this.renderFieldsUponMovieType();
         this.movie_type_selector.addEventListener('change', (e) => {
             return this.renderFiledsUponMovieTypeChange(e.target.value);
@@ -17,6 +18,7 @@ export class MovieTypeSelector {
             this.year_end.classList.remove('input-wrapper-closed');
             this.year_end.classList.add('input-wrapper-open');
             this.year_start.childNodes[1].innerText = "Year Start";
+            this.duration.childNodes[1].innerText = "Episode duration (minutes)";
         }
     }
 
@@ -27,6 +29,7 @@ export class MovieTypeSelector {
             this.year_end.classList.remove('input-wrapper-open');
             this.year_end.classList.add('input-wrapper-closed');
             this.year_start.childNodes[1].innerText = "Year";
+            this.duration.childNodes[1].innerText = "Duration (minutes)";
             this.year_end.childNodes[3].value = "";
             return true;
         }
