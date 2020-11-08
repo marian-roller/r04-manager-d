37279757899db0a0de0ac698d98cfb50e620067c
marian-roller <webmaster@rollercoastery.pl>
2020-11-08 14:49:53 +0100

    initial setup for two rich text areas in movie and mymovie forms

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,5 @@ class Movie < ApplicationRecord
   belongs_to :updated_by, class_name: 'User'
   has_many :mymovies
   has_many :users, through: :mymovies
+  has_rich_text :description
 end
--- app/models/mymovie.rb
+++ app/models/mymovie.rb
@@ -1,4 +1,5 @@
 class Mymovie < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+  has_rich_text :impression
 end
