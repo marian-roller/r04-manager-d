64c4f51ebf497e396d561cd86879cf1303d5edf7
marian-roller <webmaster@rollercoastery.pl>
2020-11-08 17:16:06 +0100

    unique mymovie.movie per user validation

--- app/models/mymovie.rb
+++ app/models/mymovie.rb
@@ -2,4 +2,6 @@ class Mymovie < ApplicationRecord
   belongs_to :movie
   belongs_to :user
   has_rich_text :impression
+  validates :movie_id, presence: true
+  validates_uniqueness_of :movie_id, scope: :user_id, message: 'already chosen'
 end
