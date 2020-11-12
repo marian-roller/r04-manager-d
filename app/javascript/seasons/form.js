24cacbebed3ca4af28247511ef95740a768dd923
marian-roller <webmaster@rollercoastery.pl>
2020-11-12 22:16:54 +0100

    first working js flow on dynamic seasons elements in movie form

--- app/javascript/seasons/form.js
+++ app/javascript/seasons/form.js
@@ -1,31 +1,43 @@
 function removeField() {
-    document.body.addEventListener("click", (e) => {
 
-        if ( e.target.nodeName === "A" && e.target.parentNode.previousElementSibling) {
-            /* to prevent from removing the first fieldset as it's previous sibling is null */
-            e.target.parentNode.remove();
-        }
-    });
+    // run only on movie form view
+    if (document.getElementById("movies-form-page")) {
+
+        const form = document.getElementById("movies-form-page");
+        form.addEventListener("click", (e) => {
+
+            // prevent from removing the first fieldset
+            if ( e.target.nodeName === "A" && e.target.parentNode.previousElementSibling) {
+                // enable deleting record from db
+                e.target.nextElementSibling.value = 1;
+                e.target.parentNode.classList.add('d-none');
+            }
+        });
+    }
 }
 
 const addBook = ()=> {
     const createButton = document.getElementById("addBook");
     createButton.addEventListener("click", () => {
-        // let items = document.querySelectorAll("#fieldsetContainer fieldset").length;
-        // console.log(items);
+
+        // preparing new record number from last fieldset id
         const lastId = document.querySelector("#fieldsetContainer fieldset:last-of-type").id;
         const newId = parseInt(lastId, 10) + 1;
-        // const newFieldset = document.querySelector('[id="0"]').outerHTML.replace(/0/g, newId);
+
+        // generate new item to insert
         const newFieldset = document.querySelector('#fieldsetContainer fieldset:first-of-type').outerHTML.replace(/0/g, newId);
 
-        // const newFieldset1 = document.querySelector("#fieldsetContainer fieldset:first-of-type");
-        // console.log(newFieldset1);
-
+        // insert new item
         document.querySelector("#fieldsetContainer").insertAdjacentHTML(
         "beforeend", newFieldset
         );
-        // items = document.querySelectorAll("#fieldsetContainer fieldset").length;
-        // console.log(items);
+
+        // setting proper values of new item
+        const lastAfterInsert = document.querySelector("#fieldsetContainer fieldset:last-of-type");
+        lastAfterInsert.getElementsByTagName('input')[0].value = parseInt(newId) + 1;
+        lastAfterInsert.getElementsByTagName('input')[1].value = '';
+        lastAfterInsert.getElementsByTagName('input')[2].value = 0;
+        console.log(lastAfterInsert.getElementsByTagName('input')[2]);
     });
 }
 export { addBook }
