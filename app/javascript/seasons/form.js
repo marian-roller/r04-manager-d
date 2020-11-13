ec217b6a0a7570c8dc2956f36ad23b2b54aab013
marian-roller <webmaster@rollercoastery.pl>
2020-11-13 21:34:58 +0100

    refactor seasons js code to new class

--- app/javascript/seasons/form.js
+++ app/javascript/seasons/form.js
@@ -1,66 +1,8 @@
-function removeSeason() {
-
-    // run only on movie form view
-    if (document.getElementById("movies-form-page")) {
-
-        const form = document.getElementById("movies-form-page");
-        form.addEventListener("click", (e) => {
-
-            // prevent from removing the first fieldset
-            // if ( e.target.nodeName === "A" && e.target.parentNode.previousElementSibling) {
-            if ( e.target.nodeName === "A" ) {
-                e.target.parentNode.classList.add('d-none');
-                // enable deleting record from db
-                e.target.nextElementSibling.value = 1;
-            }
-        });
-    }
-}
-
-const addSeason = ()=> {
-    const createButton = document.getElementById("addSeason");
-    createButton.addEventListener("click", () => {
-
-        // preparing new record number from last fieldset id
-        const lastId = document.querySelector("#fieldsetContainer fieldset:last-of-type").id;
-        const newId = parseInt(lastId, 10) + 1;
-
-        // generate new item to insert
-        const newFieldset = document.querySelector('#fieldsetContainer fieldset:first-of-type').outerHTML.replace(/0/g, newId);
-
-        let fieldsets = document.querySelectorAll('#fieldsetContainer fieldset');
-        let flag = true;
-        fieldsets.forEach( function (element, index) {
-            if (element.classList.contains('d-none')) {
-                if (flag) {
-                    element.classList.remove('d-none');
-                    flag = false;
-                } else {
-                    return;
-                }
-            }
-        });
-
-        if (flag) {
-            // insert new item
-            document.querySelector("#fieldsetContainer").insertAdjacentHTML(
-                "beforeend", newFieldset
-            );
-            // setting proper values of new item
-            let lastAfterInsert = document.querySelector("#fieldsetContainer fieldset:last-of-type");
-            let firstAfterInsert = document.querySelector("#fieldsetContainer fieldset:first-of-type");
-            lastAfterInsert.getElementsByTagName('input')[0].value = parseInt(newId) + 1;
-            lastAfterInsert.getElementsByTagName('input')[1].value = '';
-            lastAfterInsert.getElementsByTagName('input')[2].value = 0;
-        }
-    });
-}
-export { addSeason }
+import { seasonsToggler } from "./seasonsToggler";
 
 document.addEventListener('turbolinks:load', () => {
 
     if (document.querySelector('#fieldsetContainer')) {
-        addSeason();
+        new seasonsToggler;
     }
-    removeSeason();
 })
---
+++ app/javascript/seasons/seasonsToggler.js
@@ -0,0 +1,68 @@
+/*
+Class handles dynamic rendering of seasons fieldsets in movie form
+ */
+export class seasonsToggler {
+
+    constructor () {
+        this.formPage = document.getElementById("movies-form-page");
+
+        const createButton = document.getElementById("addSeason");
+        createButton.addEventListener("click", (e) => {
+            return this.addSeason();
+        })
+        this.removeSeason();
+    }
+
+    addSeason () {
+        // preparing new record number from last fieldset id
+        const lastId = document.querySelector("#fieldsetContainer fieldset:last-of-type").id;
+        const newId = parseInt(lastId, 10) + 1;
+
+        // generate new item to insert
+        const newFieldset = document.querySelector('#fieldsetContainer fieldset:first-of-type').outerHTML.replace(/0/g, newId);
+
+        const createNewItem = this.handleFieldsetsDisplay();
+
+        if (createNewItem) {
+            // insert new item
+            document.querySelector("#fieldsetContainer").insertAdjacentHTML("beforeend", newFieldset );
+            // setting proper values of new item
+            let lastAfterInsert = document.querySelector("#fieldsetContainer fieldset:last-of-type");
+            lastAfterInsert.getElementsByTagName('input')[0].value = parseInt(newId) + 1;
+            lastAfterInsert.getElementsByTagName('input')[1].value = '';
+            lastAfterInsert.getElementsByTagName('input')[2].value = 0;
+        }
+    }
+
+    removeSeason () {
+        // run only on movie form view
+        if (this.formPage) {
+            this.formPage.addEventListener("click", (e) => {
+
+                if ( e.target.nodeName === "A" ) {
+                    e.target.parentNode.classList.add('d-none');
+                    // enable deleting record - set value '1' on destroy checkbox
+                    e.target.nextElementSibling.value = 1;
+                }
+
+            });
+        }
+    }
+
+    handleFieldsetsDisplay () {
+        let fieldsets = document.querySelectorAll('#fieldsetContainer fieldset');
+        let flag = true;
+        fieldsets.forEach( (element) => {
+            if (element.classList.contains('d-none')) {
+                if (flag) {
+                    // Instead of creating new Item display first hidden one
+                    element.classList.remove('d-none');
+                    flag = false;
+                } else {
+                    return;
+                }
+            }
+        });
+        return flag;
+    }
+}
