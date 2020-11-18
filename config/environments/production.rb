3f9125b6f89e8a0f485cafe8a25fe35f7824ec08
marian-roller <webmaster@rollercoastery.pl>
2020-11-18 22:11:54 +0100

    first running stimulus action

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  #
+
+  # Use Redis store for caching.
+  # config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL_CACHING", "redis://localhost:6379/0") }
 end
