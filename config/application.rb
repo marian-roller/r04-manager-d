9ec9ee68dad3423d49433ab9a2af9ac25e1f213e
marian-roller <webmaster@rollercoastery.pl>
2020-11-27 20:48:21 +0100

    first attempt on observer, setup, not working

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.active_record.observers = :mymovie_observer
+    abort 'test'.inspect
   end
 end
