2640e2cc994032478950c93ccbdd67ed3c4eef97
marian-roller <webmaster@rollercoastery.pl>
2020-12-22 21:24:00 +0100

    frontend flow for watching start and end proper displays

--- app/javascript/mymovies/movieSelector.js
+++ app/javascript/mymovies/movieSelector.js
@@ -8,10 +8,9 @@ export class MovieSelector {
      */
     constructor () {
         this.movie = document.getElementById('mymovie_movie_id');
+        this.watching_start = document.getElementById('watching-start-field-group');
         this.watching_end = document.getElementById('watching-end-field-group');
-        this.watching_season = document.getElementById('watching-season-field-group');
-        this.last_episode = document.getElementById('last-episode-field-group');
-        this.watching_start_label = document.getElementById('watching-start-label');
+        this.watching_end_label = document.getElementById('watching-end-label');
         this.renderFieldsUponSelectedMovieType();
         this.renderFiledsUponSelectedMovieTypeChange();
         this.movie.addEventListener('change', (e) => {
@@ -26,13 +25,9 @@ export class MovieSelector {
 
         // only if not tvserie
         if (!this.renderFieldsUponSelectedMovieType()) {
-            this.watching_end.classList.remove('input-wrapper-open');
-            this.watching_end.classList.add('input-wrapper-closed');
-            this.watching_season.classList.remove('input-wrapper-open');
-            this.watching_season.classList.add('input-wrapper-closed');
-            this.last_episode.classList.remove('input-wrapper-open');
-            this.last_episode.classList.add('input-wrapper-closed');
-            this.watching_start_label.innerText = "Watched at";
+            this.watching_start.classList.remove('input-wrapper-open');
+            this.watching_start.classList.add('input-wrapper-closed');
+            this.watching_end_label.innerText = "Watched at";
         }
     }
 
@@ -44,13 +39,9 @@ export class MovieSelector {
 
         // if tvserie
         if (this.movie.options[this.movie.selectedIndex].getAttribute('data-type') === '2') {
-            this.watching_end.classList.remove('input-wrapper-closed');
-            this.watching_end.classList.add('input-wrapper-open');
-            this.watching_season.classList.remove('input-wrapper-closed');
-            this.watching_season.classList.add('input-wrapper-open');
-            this.last_episode.classList.remove('input-wrapper-closed');
-            this.last_episode.classList.add('input-wrapper-open');
-            this.watching_start_label.innerText = "Watching start";
+            this.watching_start.classList.remove('input-wrapper-closed');
+            this.watching_start.classList.add('input-wrapper-open');
+            this.watching_end_label.innerText = "Watching end";
             return true;
         }
     }
